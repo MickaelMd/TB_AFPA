5f4e65c21e686488edaf5638c771f2dd58c5b22c
MickaelMd <M1cka4l@protonmail.com>
2024-07-09 15:26:08 +0200

    maj

--- Phase_1/excercices_js/script.js
+++ Phase_1/excercices_js/script.js
@@ -6,7 +6,7 @@
 
 // alert(nom + " " + prenom);
 
-//  ----------------------------------------------------------Afficher du texte
+//  -------Afficher du texte------------------------------------------------------------
 
 // let premiernombre;
 // let deuxiemenombre;
@@ -25,7 +25,7 @@
 
 // alert(temp);
 
-// ------------------------------------------------------------------Opérateurs
+// --------Opérateurs----------------------------------------------------------
 
 // let a = 100;
 // let b = 100;
@@ -37,7 +37,7 @@
 // console.log(a + c);
 // console.log(d != d);
 
-// ---------------------------------Instructions conditionnelles et alternatives
+// -----------Instructions conditionnelles et alternatives---------------------------------
 
 // let nombre;
 // nombre = prompt("Ecrivez un nombre");
@@ -88,15 +88,108 @@
 //   alert("Erreur !, il faut utiliser un opérateur correct (+, -, *, /)");
 // }
 
-// ---------------------------------------------------------------Les boucles
+// ---Les boucles----------Pas fini------------------------------------------------------
 
-let prenom = "";
-let compteur = 0;
+// let prenom = "";
+// let compteur = 0;
 
-do {
-  prenom = prompt("Prénom ?");
-  compteur++;
-} while (prenom !== "");
-console.log(compteur);
+// do {
+//   prenom = prompt("Prénom ?");
+//   compteur++;
+// } while (prenom !== "");
+// console.log(compteur);
 
 // ----------------------------------------------------------------------------
+
+// let number;
+
+// number = prompt("Donne moi un nombre !");
+
+// while (number >= 0) {
+//   console.log(number);
+//   number--;
+// }
+
+// -----------------???????????????????????---------------------------------------
+
+// let numberone;
+// let numbertwo;
+// let numberthree;
+// let somme;
+// let result;
+// numberone = parseInt(prompt("Donne moi un nombre !"));
+// numbertwo = parseInt(prompt("Donne moi un autre nombre !"));
+// numberthree = parseInt(prompt("Donne moi encore un autre nombre !"));
+// somme = numberone + numbertwo + numberthree;
+// result = (numberone + numbertwo + numberthree) / 3;
+// console.log("La somme est de " + somme + " et la moyenne est de " + result);
+
+// while (result >= 0) {
+//   console.log(result);
+//   result--;
+// }
+
+// --------------------------------------------------------------------------------
+
+// let number;
+// let multiple;
+// let result;
+// number = parseInt(prompt("Donne moi un nombre !"));
+// multiple = parseInt(prompt("Multiplier par ?"));
+
+// do {
+//   result = number * multiple;
+//   console.log(result);
+//   number--;
+// } while (number != 0);
+// {
+//   console.log(number);
+// }
+
+// console.log();
+
+// -----------a revenir---------------------------------------------------
+
+// let mot;
+// let count = 0;
+// mot = prompt("Ecrit moi un mot");
+// for (let i = 0; i < mot.length; i++) {
+//   let voyelle = mot[i].toLowerCase();
+//   if (
+//     voyelle === "a" ||
+//     voyelle === "e" ||
+//     voyelle === "i" ||
+//     voyelle === "o" ||
+//     voyelle === "u" ||
+//     voyelle === "y"
+//   ) {
+//     count++;
+//   }
+// }
+// console.log("Nombre de voyelles : " + count);
+
+// ------Fonctions----------------------------------------------------------
+
+// function
+
+// ---------------------------------------------------------------------
+
+// function strtok(str1, str2, n) {
+
+// }
+
+// ------Tableaux---------------------------------------------------------
+
+let tableau;
+let taille;
+
+taille = parseInt(prompt("Taille du tableau ?"));
+tableau = [];
+i = taille;
+do {
+  tableau.push(prompt("mot a ajouter ?"));
+  taille = taille - 1;
+} while (taille > 0);
+console.log(tableau);
+
+// ------Le D.O.M. et l'objet---------------------------------------------
