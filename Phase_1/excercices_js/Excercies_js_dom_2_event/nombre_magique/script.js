919a59932ed42f78e22da637dd4a79492cf1aa34
Mickael <M1ck4el@protonmail.com>
2024-07-10 18:48:53 +0200

    maj

--- Phase_1/excercices_js/Excercies_js_dom_2_event/nombre_magique/script.js
+++ Phase_1/excercices_js/Excercies_js_dom_2_event/nombre_magique/script.js
@@ -7,10 +7,24 @@
 //   }
 // }
 
-let chiffre = document.getElementById("textBox1");
+// let chiffre = document.getElementById("textBox1");
 
-console.log(chiffre);
+// console.log(chiffre);
 
 // form data
 
 // https://www.youtube.com/watch?v=55EXq7ZjL4Q
+
+let input = "";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("button1").addEventListener("click", function () {
+    input = document.getElementById("textBox1").value;
+    alert(input);
+    useInputValue();
+  });
+});
+
+function useInputValue() {
+  console.log(input);
+}
