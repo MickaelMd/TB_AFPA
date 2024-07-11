4fc186bfc1a995ef35c6bc7f8f5e5ce9140b2805
Mickael <M1ck4el@protonmail.com>
2024-07-11 21:11:47 +0200

    maj

--- Phase_1/excercices_js/Les_Formulaires/assets/script.js
+++ Phase_1/excercices_js/Les_Formulaires/assets/script.js
@@ -6,6 +6,58 @@ mydropdown.onchange = function () {
   mytextbox.value = mytextbox.value + " " + mydropdownValue;
 };
 
-function checkform() {}
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-let email_error = document.getElementById("f_email");
+    if (checkform()) {
+      alert("Formulaire validé !");
+    }
+  });
+
+function checkform() {
+  let societe = document.getElementById("societe").value;
+  let personne = document.getElementById("f_personne").value;
+  let postal = document.getElementById("f_postal").value;
+  let ville = document.getElementById("f_ville").value;
+  let email = document.getElementById("f_email").value;
+
+  if (societe.length < 1) {
+    alert("La 'Société' doit comporter au moins 1 caractère");
+    return false;
+  }
+
+  if (personne.length < 1) {
+    alert("La 'Personne à contacter' doit comporter au moins 1 caractère..");
+    return false;
+  }
+
+  if (!/^\d{5}$/.test(postal)) {
+    alert("Le 'Code postal' doit comporter 5 caractères numériques.");
+    return false;
+  }
+
+  if (ville.length < 1) {
+    alert("La 'Ville' doit comporter au moins 1 caractère.");
+    return false;
+  }
+
+  if (!email.includes("@")) {
+    alert("Le Email doit comporter au moins le caractère '@'.");
+    return false;
+  }
+
+  return true;
+}
+
+// ------------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  const mouseTracker = document.getElementById("mouse-tracker");
+
+  document.addEventListener("mousemove", function (souris) {
+    mouseTracker.style.left = souris.clientX + "px";
+    mouseTracker.style.top = souris.clientY + "px";
+  });
+});
