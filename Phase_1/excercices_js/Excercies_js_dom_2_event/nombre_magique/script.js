b7bb5defc9d8258b713de21cb7bcdf5c2a3b87b7
MickaelMd <M1cka4l@protonmail.com>
2024-07-11 12:40:14 +0200

    maj

--- Phase_1/excercices_js/Excercies_js_dom_2_event/nombre_magique/script.js
+++ Phase_1/excercices_js/Excercies_js_dom_2_event/nombre_magique/script.js
@@ -1,76 +1,31 @@
-// let input = "";
-// let testinput = "test";
-// function clickclick() {
-//   document.addEventListener("DOMContentLoaded", function () {
-//     document.getElementById("button1").addEventListener("click", function (e) {
-//       input = document.getElementById("textBox1").value;
-//       inputvalue();
-//       e.preventDefault();
-//     });
-//   });
-// }
-
-// // }
-
-// function inputvalue() {
-//   console.log(input);
-// }
-
-// let number = "";
-
-// function getRandomInt(max) {
-//   return (number = Math.floor(Math.random() * max));
-// }
-
-// console.log(getRandomInt(100));
-
-// // console.log(number);
-// // console.log(input);
-// function verif(input, number) {
-//   if (input < number) {
-//     console.log("plus petit");
-//   }
-//   if (input > number) {
-//     console.log("plus grand");
-//   }
-//   if (input === number) {
-//     console.log("good");
-//   } else {
-//     console.log("error");
-//   }
-// }
-// console.log("test " + number);
-// console.log("test " + input);
-
-// -----------------------------------------------
-
 let input = "";
-let button = "";
 const number = rnumber(1, 100);
+let testtour = 100;
 
 function rnumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
 function getvalue() {
-  let input = document.getElementById("textBox1").value;
-  console.log(input);
+  input = document.getElementById("textBox1").value;
+  verif();
 }
-let testtour = 100;
+
 function verif() {
-  while (testtour > 0) {
-    if (input === number) {
-      console.log("win");
-    } else if (input < number) {
+  if (testtour > 0) {
+    const userGuess = parseInt(input);
+    if (userGuess === number) {
+      document.getElementById("label1").innerHTML = "Bravo !";
+    } else if (userGuess < number) {
       testtour--;
-      console.log("trop petit");
-    } else if (input > number) {
+      document.getElementById("label1").innerHTML = "Trop petit !";
+    } else if (userGuess > number) {
       testtour--;
-      console.log("trop grand");
+      document.getElementById("label1").innerHTML = "Trop grand !";
     }
+  } else {
+    console.log("out");
   }
 }
 
-console.log(testtour);
-console.log("chiffre " + number);
-console.log(input);
+console.log(number);
