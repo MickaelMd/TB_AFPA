dee0efdf7d70071d24279a9ef02dd6e040a1ff11
MickaelMd <M1cka4l@protonmail.com>
2024-07-11 16:27:40 +0200

    maj

--- Phase_1/excercices_js/Les_Formulaires/assets/script.js
+++ Phase_1/excercices_js/Les_Formulaires/assets/script.js
@@ -5,3 +5,7 @@ mydropdown.onchange = function () {
   let mydropdownValue = mydropdown.options[mydropdown.selectedIndex].value;
   mytextbox.value = mytextbox.value + " " + mydropdownValue;
 };
+
+function checkform() {}
+
+let email_error = document.getElementById("f_email");
