d2f0bfa3b9c2a2be6dc2984423073f71bff174bc
MickaelMd <M1cka4l@protonmail.com>
2024-07-11 11:28:23 +0200

    maj

--- Phase_1/excercices_js/Excercies_js_dom_2_event/nombre_magique/script.js
+++ Phase_1/excercices_js/Excercies_js_dom_2_event/nombre_magique/script.js
@@ -1,30 +1,71 @@
-// function getmsg() {
-//   let input = document
-//     .getElementById("textBox1")
-//     .addEventListener("click", value);
-//   {
-//     alert(input);
-//   }
+// let input = "";
+// let testinput = "test";
+// function clickclick() {
+//   document.addEventListener("DOMContentLoaded", function () {
+//     document.getElementById("button1").addEventListener("click", function (e) {
+//       input = document.getElementById("textBox1").value;
+//       inputvalue();
+//       e.preventDefault();
+//     });
+//   });
 // }
 
-// let chiffre = document.getElementById("textBox1");
+// // }
 
-// console.log(chiffre);
+// function inputvalue() {
+//   console.log(input);
+// }
 
-// form data
+// let number = "";
 
-// https://www.youtube.com/watch?v=55EXq7ZjL4Q
+// function getRandomInt(max) {
+//   return (number = Math.floor(Math.random() * max));
+// }
+
+// console.log(getRandomInt(100));
+
+// // console.log(number);
+// // console.log(input);
+// function verif(input, number) {
+//   if (input < number) {
+//     console.log("plus petit");
+//   }
+//   if (input > number) {
+//     console.log("plus grand");
+//   }
+//   if (input === number) {
+//     console.log("good");
+//   } else {
+//     console.log("error");
+//   }
+// }
+// console.log("test " + number);
+// console.log("test " + input);
+
+// -----------------------------------------------
 
 let input = "";
+let button = "";
+let number = rnumber(1, 100);
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("button1").addEventListener("click", function () {
-    input = document.getElementById("textBox1").value;
-    alert(input);
-    useInputValue();
-  });
-});
-
-function useInputValue() {
-  console.log(input);
+function rnumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
+
+function getvalue() {
+  var n = document.getElementById("textBox1").value;
+  console.log(n);
+}
+
+function verif() {
+  if (input == number) {
+    console.log("good");
+  } else if (input < number) {
+    console.log("trop petit");
+  } else if (input > number) {
+    console.log("trop grand");
+  }
+}
+
+console.log("chiffre " + number);
+console.log(input);
