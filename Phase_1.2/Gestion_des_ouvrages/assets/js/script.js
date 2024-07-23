bae7fb49853d5ae769d5c10097a5bad2582364a0
MickaelMd <M1cka4l@protonmail.com>
2024-07-23 10:15:08 +0200

    add

--- Phase_1.2/Gestion_des_ouvrages/assets/js/script.js
+++ Phase_1.2/Gestion_des_ouvrages/assets/js/script.js
@@ -5,13 +5,13 @@ const list_authors = data.map((book) => book.authors);
 const all_authors = [].concat(...list_authors);
 const newlist_authors = [...new Set(all_authors)];
 newlist_authors.sort((a, b) => a.localeCompare(b));
-console.log(newlist_authors);
+// console.log(newlist_authors);
 
 const list_categories = data.map((book) => book.categories);
 const all_categories = [].concat(...list_categories);
 const newlist_categories = [...new Set(all_categories)];
 newlist_categories.sort((a, b) => a.localeCompare(b));
-console.log(newlist_categories);
+// console.log(newlist_categories);
 
 const authors_select = document.getElementById("auteurs_select");
 const categorie_select = document.getElementById("categorie_select");
@@ -36,7 +36,13 @@ authors_select.addEventListener("click", () => {
   categorie_select.value = "";
 });
 
-function testdisplay(data) {
+// ----------------------------------------------------
+
+// function filtre() {}
+
+// ----------------------------------------------------
+
+function displaybook(data) {
   let listelivres = document.getElementById("displaycards");
   listelivres.innerHTML = "";
 
@@ -45,28 +51,67 @@ function testdisplay(data) {
     displaybook.className = "card";
     listelivres.appendChild(displaybook);
 
+    // ---------------------------------------
+
     let thumbnail = document.createElement("img");
+    // if (book.thumbnailUrl == undefined) {
+    //   thumbnail.src =
+    //     "https://p1.storage.canalblog.com/14/48/1145642/91330992_o.png";
+    // } else {
+    //   thumbnail.src = book.thumbnailUrl;
+    //   displaybook.appendChild(thumbnail);
+    // }
     thumbnail.src = book.thumbnailUrl;
-    thumbnail.alt = book.title;
-    thumbnail.className = "thumbnail";
     displaybook.appendChild(thumbnail);
 
+    console.log(book.thumbnailUrl);
+
+    // ---------------------------------------
+
     let nomlivre = document.createElement("h4");
-    nomlivre.textContent = `Titre: ${book.title}`;
+    nomlivre.textContent = `${book.title}`;
     displaybook.appendChild(nomlivre);
 
+    let isbn = document.createElement("p");
+    isbn.textContent = `ISBN : ${book.isbn}`;
+    displaybook.appendChild(isbn);
+
+    let datepubli = document.createElement("p");
+    if (book.publishedDate == null) {
+      datepubli.textContent = "";
+    } else {
+      datepubli.textContent = `Date de publication : ${book.publishedDate.dt_txt}`;
+      // datepubli.textContent = `Date de publication : ${date.book.publishedDate.dt_txt.setUTCDate()}`;
+    }
+    displaybook.appendChild(datepubli);
+
+    let numberpage = document.createElement("p");
+    if (book.pageCount == 0) {
+      numberpage.textContent = "";
+    } else {
+      numberpage.textContent = `Nombre de pages : ${book.pageCount}`;
+    }
+    displaybook.appendChild(numberpage);
+
     let auteur = document.createElement("p");
-    auteur.textContent = `Auteur(s): ${book.authors.join(", ")}`;
+    auteur.textContent = `Auteur : ${book.authors.join(", ")}`;
     displaybook.appendChild(auteur);
 
     let categorie = document.createElement("p");
-    categorie.textContent = `Catégorie(s): ${book.categories.join(", ")}`;
+    categorie.textContent = `Catégorie : ${book.categories.join(", ")}`;
     displaybook.appendChild(categorie);
 
     let description = document.createElement("p");
-    description.textContent = `Description: ${book.shortDescription}`;
+    if (book.shortDescription == null) {
+      categorie.textContent = "";
+    } else {
+      description.textContent = ` Description : ${book.shortDescription}`;
+    }
     displaybook.appendChild(description);
   });
 }
 
-testdisplay(data);
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+displaybook(data);
